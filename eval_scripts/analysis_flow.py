a4300c31b684f7d6250bc02b4a8502a63155f07d
laurence <laurence0636@outlook.com>
2022-07-08 10:11:25 +0800

    feat(APP-Result): Revise codes based on introducing "can_gs".

--- eval_scripts/analysis_flow.py
+++ eval_scripts/analysis_flow.py
@@ -11,12 +11,17 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import tensorflow as tf
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+# tf.get_logger().setLevel('INFO')
+tf.get_logger().setLevel(logging.ERROR)
+
 PROJ_PATH = '/mnt/c/Users/Laurence_Liu/Documents/Regtics_proj/NWA_AI'
 if PROJ_PATH not in sys.path:
     sys.path.append(PROJ_PATH)
 from graphsage.supervised_train import load_data, train, FLAGS
 from eval_scripts import check_node_feat, post_graphsage_trained
 from graphsage.minibatch import NodeMinibatchIterator
+from networkx.readwrite import json_graph
+
 log = logging.getLogger('nwa.final.analysis.com')
 # sys.stdout = StreamToLogger(log,logging.INFO)
 # sys.stderr = StreamToLogger(log,logging.ERROR)
@@ -44,14 +49,22 @@ def lassert(args):
 
 exp_config = {
     'global': {
-        'train_prefix': "../example_data/livi-test-data-20220511",
-        'gs_train_prefix': "../example_data/livi-test-data-20220511",
-        'model_path': "/mnt/c/Users/Laurence_Liu/Documents/Regtics_proj/NWA_AI/sup-example_data/graphsage_mean_small_0.0010_20-05-2022-10:34:09/weights.003-sb@0-22f1_mic-0.969-f1_mac-0.706",
+        # 'train_prefix': "../example_data/data_2022-06-29_2322",
+        # 'train_prefix': "../example_data/data_2022-06-01_0956",
+        # 'train_prefix': "../example_data/livi-test-data-20220511",
+        'gs_train_prefix': "../example_data/data_2022-06-29_2322-trim",
+        # 'gs_train_prefix': "../example_data/data_2022-06-01_0956",
+        # 'gs_train_prefix': "../example_data/livi-test-data-20220511",
+        # 'model_path': "/mnt/c/Users/Laurence_Liu/Documents/Regtics_proj/NWA_AI/sup-example_data/graphsage_mean_small_0.0010_22-06-2022-22:16:31/weights.049-sb@0-77f1_mic-0.922-f1_mac-0.922",
+        # 'model_path': "/mnt/c/Users/Laurence_Liu/Documents/Regtics_proj/NWA_AI/sup-example_data/graphsage_mean_small_0.0010_20-05-2022-10:34:09/weights.003-sb@0-22f1_mic-0.969-f1_mac-0.706",
+        # 'model_path': "/mnt/c/Users/Laurence_Liu/Documents/Regtics_proj/NWA_AI/sup-example_data/graphsage_mean_small_0.0010_30-06-2022-01:36:55/weights.120-sb@0-56f1_mic-0.949-f1_mac-0.949",
+        # 'model_path': "/mnt/c/Users/Laurence_Liu/Documents/Regtics_proj/NWA_AI/sup-example_data/graphsage_mean_small_0.0010_05-07-2022-23:42:12/weights.086-sb@0-57f1_mic-0.000-f1_mac-0.000",
+        'model_path':'/mnt/c/Users/Laurence_Liu/Documents/Regtics_proj/NWA_AI/sup-example_data/graphsage_mean_small_0.0010_06-07-2022-10:43:35/weights.001-sb@0-20f1_mic-0.000-f1_mac-0.000',
         'PROJ_PATH' : PROJ_PATH,
         'yyyymmdd_HHMM': datetime.today().strftime('%Y%m%d_%H%M'),
         'NB_ITER': 3,
         'set_FLAGS': lambda : (
-            FLAGS.mark_as_parsed(), 
+            FLAGS.mark_as_parsed(),
             setattr(FLAGS, 'train_prefix', exp_config['global']['gs_train_prefix']),
             setattr(FLAGS, 'sigmoid', False),
             setattr(FLAGS, 'train_return_model_once_ready', True),
@@ -71,7 +84,10 @@ exp_config = {
         'set_df_G_data': lambda : (
             minibatch_it := exp_config['global']['minibatch_it'],
             G_data := json.load(open(FLAGS.train_prefix + "-G.json")),
-            can_graph_dict := {n: 1 for n in minibatch_it.train_nodes + minibatch_it.val_nodes + minibatch_it.test_nodes},
+            # Note that train/val/testing nodes are subset of "can_graph" nodes, as some can_graph nodes containing 95% fake nodes and no other real nodes, 
+            # which are correct but will be remove from trai/val/testing nodes. 
+            # can_graph_dict := {n: 1 for n in minibatch_it.train_nodes + minibatch_it.val_nodes + minibatch_it.test_nodes},
+            can_graph_dict := {n: 1 for n in minibatch_it.G.nodes if (minibatch_it.G.nodes[n]['can_graph']==1)and(minibatch_it.G.nodes[n]['real']==True)},
             df_G_data := pd.DataFrame(),
             df_G_data := df_G_data.assign(
                 id=[n['id'] for n in G_data['nodes'] if n.setdefault('real', False)],
@@ -80,6 +96,10 @@ exp_config = {
                 val = [n['val'] for n in G_data['nodes'] if n.setdefault('real', False)],
                 test = [n['test'] for n in G_data['nodes'] if n.setdefault('real', False)],
                 can_graph = [can_graph_dict.setdefault(n['id'], 0) for n in G_data['nodes'] if n.setdefault('real', False) ],
+            ),
+            # Laurence 20220706
+            df_G_data := df_G_data.assign(
+                node_feat_check_sum=lambda x: [sum(e) for e in x['feat']]
             ),
             exp_config['global'].setdefault('df_G_data', df_G_data)
         ),
@@ -93,8 +113,17 @@ exp_config = {
             graph_pred_raw_name := f"graph_pred_raw_{exp_config['global']['NB_ITER']}",
             df_combine_info := pd.merge(
                 left=exp_config['global']['df_G_data'],
-                right=exp_config['global']['df_gs_info'][['id','graph_feat','graph_pred',graph_feat_agg_name,graph_pred_raw_name]],
-                on='id', how='left'),
+                right=exp_config['global']['df_gs_info'][['id','graph_feat','graph_pred','node_feat_check_sum',graph_feat_agg_name,graph_pred_raw_name]],
+                on=['id','node_feat_check_sum'], how='left'),
+
+            # df_combine_info := pd.merge(
+            #     left=exp_config['global']['df_G_data'],
+            #     right=exp_config['global']['df_gs_info'][['id','graph_feat','graph_pred',graph_feat_agg_name,graph_pred_raw_name]],
+            #     on='id', how='left'),
+
+
+            # Laurence 20220706
+            df_combine_info := df_combine_info.assign(can_gs=lambda x: ~x['graph_feat'].isna()),
             lassert(df_combine_info['can_graph'].isna().sum() == 0),
             # exp_config['global'].setdefault('df_combine_info', df_combine_info),
             exp_config['global'].update({'df_combine_info': df_combine_info}),
@@ -102,9 +131,13 @@ exp_config = {
         'pad_zero_df_combine_info': lambda : (
             graph_feat_agg_name := f"graph_feat_agg_{exp_config['global']['NB_ITER']}",
             df_combine_info := exp_config['global']['df_combine_info'],
-            pad_zero_idx := df_combine_info.query('can_graph==0').index,
+            # pad_zero_idx := df_combine_info.query('can_graph==0').index,
+            pad_zero_idx := df_combine_info.query('can_gs==False').index,
+            pad_zero_idx_to_assert := df_combine_info.pipe(lambda x: x[x['graph_feat'].isna()]).index,
             pad_zero_ind_dict := {v:True for v in pad_zero_idx.values},
-            graph_feat_dim := len(df_combine_info.query("can_graph==1")['graph_feat'].values[0]),
+            # Laurence 20220706
+            # graph_feat_dim := len(df_combine_info.query("can_graph==1")['graph_feat'].values[0]),
+            graph_feat_dim := len(df_combine_info.query("can_gs==True")['graph_feat'].values[0]),
             zero_feat := [0.] * graph_feat_dim,
             graph_feat_agg_col_val := [e if pad_zero_ind_dict.setdefault(k, False)==False else zero_feat for k,e in df_combine_info[graph_feat_agg_name].items() ],
             graph_feat_col_val := [e if pad_zero_ind_dict.setdefault(k, False)==False else zero_feat for k,e in df_combine_info['graph_feat'].items() ],
@@ -117,8 +150,10 @@ exp_config = {
             df_G_data := exp_config['global']['df_G_data'],
             df_gs_info := exp_config['global']['df_gs_info'],
             df_combine_info := exp_config['global']['df_combine_info'],
-            lassert(df_G_data.query("(can_graph==1)").shape[0] == df_gs_info.query("is_graph==True").shape[0]),
-            lassert(df_combine_info['graph_feat'].isna().sum() == 0),
+            # Laurence 20220706
+            # lassert(df_G_data.query("(can_graph==1)").shape[0] == df_gs_info.query("is_graph==True").shape[0]),
+            lassert(df_G_data.query("(can_graph==1)").shape[0] >= df_gs_info.query("is_graph==True").shape[0]),
+            lassert(df_combine_info['graph_feat'].isna().sum() == 0), # Since we apply "pad_zero_df_combine_info"
             lassert(df_combine_info[graph_feat_agg_name].isna().sum() == 0),
         ),
         'set_node_feat_append_graph_feat_info': lambda : (
@@ -155,7 +190,9 @@ exp_config = {
         ),
         'compute_gs_rdr_info': lambda : (
             df_combine_info := exp_config['global']['df_combine_info'],
-            ms := df_combine_info.query("(can_graph==1)&(test==True)&(label==1.0)").pipe(lambda x: np.stack(x['graph_pred'])[:,1] ).min(),
+            # Laurence 20220706
+            # ms := df_combine_info.query("(can_graph==1)&(test==True)&(label==1.0)").pipe(lambda x: np.stack(x['graph_pred'])[:,1] ).min(),
+            ms := df_combine_info.query("(can_gs==1)&(test==True)&(label==1.0)").pipe(lambda x: np.stack(x['graph_pred'])[:,1] ).min(),
             log.info(f'minimum "can graph" true-hit score from GS: {ms}'),
             rdr_nb := df_combine_info.query("(can_graph==1)&(test==True)").pipe(lambda x: np.stack(x['graph_pred'])[:,1] < ms).sum(),
             log.info(f'reduction number on "can graph" testing data: {rdr_nb}'),
@@ -209,7 +246,7 @@ exp_config = {
         ],
         # Note: the load_and_check_model_wegiths should place on top as it has "in-place" effect.
         'run_exp_ls' : [
-            'load_and_check_model_weights',
+            # 'load_and_check_model_weights',
             'one_LR_w_feat_on_all', 
             'one_LR_w_feat_on_can_graph',
             'one_LR_w_feat_on_can_not_graph',
@@ -257,8 +294,9 @@ exp_config = {
         'help':'Load model checkpoint, and then verify its validation by pre-saved data file (e.g., check_sb@0.npy. Note that the last element in sb_k will be used to load model weights. In particular, we use "feat".',
         'type': 'verify_model_weight',
         # 'model': model,
-        'checkpoint_path': "/mnt/c/Users/Laurence_Liu/Documents/Regtics_proj/NWA_AI/sup-example_data/graphsage_mean_small_0.0010_20-05-2022-10:34:09",
-        'sb_k': [0], #  2, 4, 8
+        # 'checkpoint_path': "/mnt/c/Users/Laurence_Liu/Documents/Regtics_proj/NWA_AI/sup-example_data/graphsage_mean_small_0.0010_20-05-2022-10:34:09",
+        # 'checkpoint_path': "/mnt/c/Users/Laurence_Liu/Documents/Regtics_proj/NWA_AI/sup-example_data/graphsage_mean_small_0.0010_06-07-2022-10:43:35", # Laurence 20220706
+        # 'sb_k': [0], #  2, 4, 8
     },
     'one_LR_w_graph_feat_on_can_graph':{
         'help':'Use one single logistic regression (LR) on "can graph" data using graphsage feature. In particular, we use "feat".',
@@ -355,24 +393,24 @@ if __name__ == '__main__':
             logreg = check_node_feat.fit_lr(feat_tr, target_tr, **kwargs)
             check_node_feat.lr_eval(logreg, feat_tr, feat_te, target_tr, target_te, log=log)
             
-        if e['type'] == 'verify_model_weight':
-            assert 'train_data' in exp_config['global'].keys()
-            model = train(exp_config['global']['train_data'])
+        # if e['type'] == 'verify_model_weight':
+        #     assert 'train_data' in exp_config['global'].keys()
+        #     model = train(exp_config['global']['train_data'])
 
-            sb_k_ls = e['sb_k']
-            # model = e['model']
-            checkpoint_path = e['checkpoint_path']
+        #     sb_k_ls = e['sb_k']
+        #     # model = e['model']
+        #     checkpoint_path = e['checkpoint_path']
 
-            for sk in sb_k_ls:
-                check_sb_data = np.load(f'{checkpoint_path}/check_sb@{sk}.npy', allow_pickle=True).item()
-                check_sb_result = np.load(f'{checkpoint_path}/check_sb@{sk}_result.npy', allow_pickle=True)
-                model_path = glob.glob(f"{checkpoint_path}/weights.{check_sb_result[-1]['epoch']:03d}-sb@{sk}*.index")[0].split('.index')[0]
-                log.info(f"model_path (best with sb@{sk}): {model_path}")
-                model.load_weights(model_path)
-                model.check_sb_data = check_sb_data
-                pred_rs = model.test_one_step(check_sb_data, return_node_feat=True)
-                assert (check_sb_result[0] == pred_rs[0].numpy()).all()
-                log.info(f'Model (best with sb@{sk}) weights verified (passed).\n')
+        #     for sk in sb_k_ls:
+        #         check_sb_data = np.load(f'{checkpoint_path}/check_sb@{sk}.npy', allow_pickle=True).item()
+        #         check_sb_result = np.load(f'{checkpoint_path}/check_sb@{sk}_result.npy', allow_pickle=True)
+        #         model_path = glob.glob(f"{checkpoint_path}/weights.{check_sb_result[-1]['epoch']:03d}-sb@{sk}*.index")[0].split('.index')[0]
+        #         log.info(f"model_path (best with sb@{sk}): {model_path}")
+        #         model.load_weights(model_path)
+        #         model.check_sb_data = check_sb_data
+        #         pred_rs = model.test_one_step(check_sb_data, return_node_feat=True)
+        #         # assert (check_sb_result[0] == pred_rs[0].numpy()).all()
+        #         log.info(f'Model (best with sb@{sk}) weights verified (passed).\n')
 
         log.info('+' * 100)
 
